cd06bb4c74cfe45212cbaaf5ebfc15d0347fada4
MernJs Community <mernjscommunity@gmail.com>
2021-06-26 18:46:56 +0530

    REACTJS TS FOLDER RE-STRUCTURE

--- templates/typescript/reactjs-setup/reactjs-setup-with-redux-saga-with-api-mysql-sql-query/server.js
+++ templates/typescript/reactjs-setup/reactjs-setup-with-redux-saga-with-api-mysql-sql-query/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
-global.db           = require('./src/database/dbconfig/DB_mysql_connection');
-const Helpers       = require('./src/helpers/Helpers')
+global.db           = require('./server/database/dbconfig/DB_mysql_connection');
+const Helpers       = require('./server/helpers/Helpers')
 const bodyParser    = require('body-parser')
 const express       = require('express')
 const logger        = require('morgan')
@@ -15,13 +15,13 @@ app.use(bodyParser.json({limit: '10mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './src/views'));
-app.use(express.static(path.join(__dirname, './public')));
+app.set('views', path.join(__dirname, './server/views'));
+// app.use(express.static(path.join(__dirname, './public')));
 
-fs.readdirSync(path.join(__dirname, `./src/routes`)).forEach(function(file) {
+fs.readdirSync(path.join(__dirname, `./server/routes`)).forEach(function(file) {
     if (file.match(/\.js$/) !== null && file !== 'index.js') {
         let name = file.replace('.js', '');
-        app.use(require(`./src/routes/${name}`));
+        app.use(require(`./server/routes/${name}`));
     }
 });
 
