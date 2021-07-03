609ab7969302e7d7abd3dd54b7c73de7a75d3010
MernJs Community <mernjscommunity@gmail.com>
2021-07-03 14:40:53 +0530

    Upgrade NodeJs Packages

--- templates/default/nodejs-setup/nodejs-setup-with-mysql-bookshelf-orm/src/database/dbconfig/DB_knex_connection.js
+++ templates/default/nodejs-setup/nodejs-setup-with-mysql-bookshelf-orm/src/database/dbconfig/DB_knex_connection.js
@@ -1,7 +1,5 @@
 const connection = require('./DB_mysql_config')
-const knexPaginator = require('knex-paginator')
 
 const Knex = require('knex')(connection);
-knexPaginator(Knex);
 
 module.exports = Knex;
