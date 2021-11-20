00338b1ae8d3b730e561921b4f54e3ed9589f712
MernJs Community <mernjscommunity@gmail.com>
2021-11-20 16:30:45 +0530

    update

---
+++ config.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#NextJs
+rm -rf templates/nextjs-redux
+heroku git:clone -a mernjs-nextjs templates/nextjs-redux
+cd templates/nextjs-redux
+
+
+#NodeJs
+rm -rf templates/nodejs-expressjs
+heroku git:clone -a mernjs-nodejs templates/nodejs-expressjs
+
+
+#Context
+rm -rf templates/reactjs-context
+heroku git:clone -a mernjs-react-context templates/reactjs-context
+
+
+#ElectronJs
+rm -rf templates/reactjs-electronjs
+heroku git:clone -a mernjs-electronjs templates/reactjs-electronjs
+
+
+#ReactJs
+rm -rf templates/reactjs-redux
+heroku git:clone -a mernjs-reactjs templates/reactjs-redux
