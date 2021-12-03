0bb2827d89ca883701ca9efea313b299e52967d5
MernJs Community <mernjscommunity@gmail.com>
2021-12-04 00:59:30 +0530

    Update...

--- docs/blog/src/database/dbconfig/DB_config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-let ENV = {}
-ENV.MONGO_URI=process.env.MONGO_URI
-ENV.DB_MIGRATION = process.env.DB_MIGRATION = 'migration';
-ENV.APP_DEBUG = process.env.APP_DEBUG === "true" ? true : false,
-module.exports = ENV
