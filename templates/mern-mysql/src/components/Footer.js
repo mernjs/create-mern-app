65ed27df73cea8690a6d093937b344d3b402a9cf
MernJs Community <mernjscommunity@gmail.com>
2022-03-20 21:22:46 +0530

    Update

--- templates/mern-mysql/src/components/Footer.js
+++ templates/mern-mysql/src/components/Footer.js
@@ -6,7 +6,7 @@ export default (props) => {
         <Footer>
             <div className="container">
                 <div className="row">
-                    <p style={{marginLeft: '30px', marginRight: '30px'}}><span style={{textAlign: 'left', lineHeight: '60px'}}>©2021 Create MERN App</span><span style={{float: 'right', lineHeight: '60px'}}><b>By:</b> Vijay Pratap Singh</span></p>
+                <p style={{marginLeft: '30px', marginRight: '30px'}}><span style={{textAlign: 'left', lineHeight: '60px'}}>©2021 <a href="https://mernjs.github.io/create-mern-app" target="_blank"> Create MERN App</a></span><span style={{float: 'right', lineHeight: '60px'}}><b>By:</b> <a href="https://vijay-pratap-singh.netlify.app" target="_blank"> Vijay Pratap Singh</a></span></p>
                 </div>
             </div>
         </Footer>
