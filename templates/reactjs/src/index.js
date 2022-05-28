db3a8daf44081d00a8ffae02f2e1526029b29ded
MernJs Community <mernjscommunity@gmail.com>
2022-05-28 17:25:14 +0530

    Downgrad react & ract-dom version

--- templates/reactjs/src/index.js
+++ templates/reactjs/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 ``
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
