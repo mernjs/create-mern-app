43efd7a8cf678b7ae2a85fafb14fdc8cb6227e9d
MernJs Community <mernjscommunity@gmail.com>
2022-08-31 22:36:13 +0530

    Disabled NextJS PWA

--- templates/mern-mysql/next.config.js
+++ templates/mern-mysql/next.config.js
@@ -1,12 +1,12 @@
-const withPWA = require("next-pwa");
+// const withPWA = require("next-pwa");
 
-console.log('process.env.NODE_ENV', process.env)
+// console.log('process.env.NODE_ENV', process.env)
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-});
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === "development",
+//   },
+// });
--- templates/nextjs/next.config.js
+++ templates/nextjs/next.config.js
@@ -1,10 +1,10 @@
-const withPWA = require("next-pwa");
+// const withPWA = require("next-pwa");
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-});
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === "development",
+//   },
+// });
