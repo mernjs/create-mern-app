3c55e2616dea2ba61d7b7219e973a94262a7caea
MernJs Community <mernjscommunity@gmail.com>
2022-11-26 20:19:46 +0530

    Update

--- templates/boilerplates/mern-boilerplate/src/pages/_document.js
+++ templates/boilerplates/mern-boilerplate/src/pages/_document.js
@@ -12,7 +12,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#000000" />
           <meta
