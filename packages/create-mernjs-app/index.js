e875daf364762b5a3ac9d66009358c32c376671d
MernJs Community <mernjscommunity@gmail.com>
2022-11-29 01:16:39 +0530

    Update NodeJS Version

--- packages/create-mernjs-app/index.js
+++ packages/create-mernjs-app/index.js
@@ -6,13 +6,13 @@ var currentNodeVersion = process.versions.node;
 var semver = currentNodeVersion.split('.');
 var major = semver[0];
 
-if (major < 10) {
+if (major < 14) {
   console.error(
     'You are running Node ' +
-      currentNodeVersion +
-      '.\n' +
-      'Create MernJs App requires Node 10 or higher. \n' +
-      'Please update your version of Node.'
+    currentNodeVersion +
+    '.\n' +
+    'Create MERN App requires Node 14 or higher. \n' +
+    'Please update your version of Node.'
   );
   process.exit(1);
 }
