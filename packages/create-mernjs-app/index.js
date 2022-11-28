5e6438601d40b3a39fced6e0ce58764a55d5a1f7
MernJs Community <mernjscommunity@gmail.com>
2022-11-29 01:44:39 +0530

    Publish 17.0.2

--- packages/create-mernjs-app/index.js
+++ packages/create-mernjs-app/index.js
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
 
 'use strict';
-
+const chalk = require("chalk");
+const yosay = require('yosay')
 var currentNodeVersion = process.versions.node;
 var semver = currentNodeVersion.split('.');
 var major = semver[0];
 
 if (major < 14) {
-  console.error(
-    'You are running Node ' +
-    currentNodeVersion +
-    '.\n' +
-    'Create MERN App requires Node 14 or higher. \n' +
-    'Please update your version of Node.'
-  );
+  const message = `Create MERN App requires Node 14 or higher.`
+  console.log('\n')
+  console.log(yosay(`${chalk.red(`You are running Node ${currentNodeVersion}.`)} \n ${chalk.green(' ')} \n ${message} \n\n ${chalk.bold.blue('Please update your version of Node.')} \n ${chalk.green(' ')} `, { maxLength: 55 }));
+  console.log('\n')
   process.exit(1);
 }
 
