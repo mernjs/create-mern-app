5809dc5993226a246db2122d6103935682152365
MernJs Community <mernjscommunity@gmail.com>
2023-01-20 21:18:08 +0530

    UPDATE

--- templates/boilerplates/new-gatsbyjs-boilerplate/gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-const App = require('./src/App');
-
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-    setHtmlAttributes({ lang: `en` });
-};
-
-exports.wrapPageElement = ({ element, props }) => {
-    return <App {...props}>{element}</App>;
-};
