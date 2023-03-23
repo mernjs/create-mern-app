5b65b410466080262c0578d2d015b24953f57ccb
MernJs Community <mernjscommunity@gmail.com>
2023-03-24 00:34:20 +0530

    Update

--- templates/app/gatsbyjs-boilerplate/src/pages/index.js
+++ templates/app/gatsbyjs-boilerplate/src/pages/index.js
@@ -4,31 +4,31 @@ import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 
 const Dashboard = () => {
-	const user = useSelector((state) => state.auth.user);
-	return (
-		<>
-			<Head title={"Home"} />
-			<ScrollView>
-				<Header />
-				<Container>
-					{!user ? (
-						<h1>Home</h1>
-					) : (
-						<>
-							<h1>Dashboard</h1>
-							<p>
-								<b>Name</b>: {user?.name || ''}
-							</p>
-							<p>
-								<b>Email</b>: {user?.email || ''}
-							</p>
-						</>
-					)}
-				</Container>
-			</ScrollView>
-			<Footer />
-		</>
-	);
+    const user = useSelector((state) => state.auth.user);
+    return (
+        <>
+            <Head title={""} />
+            <ScrollView>
+                <Header />
+                <Container>
+                    {!user ? (
+                        <h1>Home</h1>
+                    ) : (
+                        <>
+                            <h1>Dashboard</h1>
+                            <p>
+                                <b>Name</b>: {user?.name || ''}
+                            </p>
+                            <p>
+                                <b>Email</b>: {user?.email || ''}
+                            </p>
+                        </>
+                    )}
+                </Container>
+            </ScrollView>
+            <Footer />
+        </>
+    );
 };
 
 export default Dashboard;
