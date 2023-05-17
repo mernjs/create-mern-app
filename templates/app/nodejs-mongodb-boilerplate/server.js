5d52e78c305d6045aa0378d9538964c178db7228
MernJs Community <mernjscommunity@gmail.com>
2023-05-18 00:04:19 +0530

    Add fastify boilerplate

--- templates/app/nodejs-mongodb-boilerplate/server.js
+++ templates/app/nodejs-mongodb-boilerplate/server.js
@@ -25,15 +25,14 @@ app.disable('x-powered-by');
 const server = app
     .listen(process.env.PORT || process.env.APP_PORT, () => {
         console.log(
-            `********** Server is running on  http://localhost:${
-                server.address().port
+            `********** Server is running on  http://localhost:${server.address().port
             }  **********`,
         );
     })
     .on('error', (error) => {
         console.log(
             '********** \x1b[31mPort ' +
-                error.port +
-                ' is already in use\x1b[0m **********',
+            error.port +
+            ' is already in use\x1b[0m **********',
         );
     });
