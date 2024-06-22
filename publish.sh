8439d62c57881ab7cc5eeefcb670689599ac8944
Vijay Pratap <vijaypratap218@gmail.com>
2024-06-22 16:57:32 +0530

    Y2024

--- publish.sh
+++ publish.sh
@@ -30,8 +30,11 @@ check_last_command
 # Step 4: Bump the version (patch, minor, or major)
 # Update the version here as per your need
 echo "Updating the package version..."
-npm version patch  # Use npm version minor or npm version major as needed
+NEW_VERSION=$(npm version patch)  # Use npm version minor or npm version major as needed
 check_last_command
+
+# Extract the new version tag
+NEW_VERSION_TAG=$(echo $NEW_VERSION | tr -d 'v')
 
 # Step 5: Ensure you are logged into npm
 echo "Checking npm login status..."
@@ -49,7 +52,16 @@ check_last_command
 
 echo "Package published successfully!"
 
-# Step 7: Push the version bump commit and tag to the remote repository
+# Step 7: Create and push the version tag to GitHub
+echo "Creating a new Git tag for the version $NEW_VERSION_TAG..."
+git tag -a "v$NEW_VERSION_TAG" -m "Release version $NEW_VERSION_TAG"
+check_last_command
+
+echo "Pushing the tag to GitHub..."
+git push origin "v$NEW_VERSION_TAG"
+check_last_command
+
+# Step 8: Push the version bump commit and tag to the remote repository
 echo "Pushing version bump commit and tag to the remote repository..."
 git push origin master --follow-tags
 check_last_command
