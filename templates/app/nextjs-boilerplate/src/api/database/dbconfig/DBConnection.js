6a1b88b2b805fb1b303f3648a1d569211f6ab69b
Vijay Pratap <vijaypratap218@gmail.com>
2024-06-30 20:08:05 +0530

    Y2024

--- templates/app/nextjs-boilerplate/src/api/database/dbconfig/DBConnection.js
+++ templates/app/nextjs-boilerplate/src/api/database/dbconfig/DBConnection.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 const ENV = require('./DBConfig');
 mongoose.Promise = global.Promise;
 
-mongoose
-    .connect(ENV.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('********** Successfully Connected To The MongoDB **********');
-    })
-    .catch((err) => {
-        console.error('********** Missing DB Connection ***********');
-    });
+mongoose.connect(ENV.MONGO_URI).then(() => {
+    console.log('********** Successfully Connected To The MongoDB **********');
+}).catch((err) => {
+    console.error('********** Missing DB Connection ***********');
+});
